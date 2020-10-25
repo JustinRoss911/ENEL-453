321756067bc32db2ad28b8a978d99fd9ca095749
jesscucu <72361774+jesscucu@users.noreply.github.com>
2020-10-24 18:00:59 -0600

    downto 0

--- Lab 2/MUX4To1.vhd
+++ Lab 2/MUX4To1.vhd
@@ -10,7 +10,7 @@ port ( in1     : in  std_logic_vector(15 downto 0); -- in1 = binary (hex)
        in2     : in  std_logic_vector(15 downto 0); -- in2 = decimal 
 		 in3     : in  std_logic_vector(15 downto 0); -- in3 = stored value 
 		 in4     : in  std_logic_vector(15 downto 0); -- in4 = 5A5A
-       s       : in  std_logic_vector; -- Switches that toggles between mode
+       s       : in  std_logic_vector(1 downto 0);; -- Switches that toggles between mode
        mux_out : out std_logic_vector(15 downto 0)  -- output bits 
       );
 end MUX4TO1;
@@ -19,11 +19,11 @@ end MUX4TO1;
 ----------------------------------------------------------------
 architecture behaviour of MUX4TO1 is
 	begin
-		with s select
-			mux_out <= in1 when "00",
-			           in2 when "01",
-						  in3 when "10",
-						  in4 when others;
+		with s select mux_out <= 
+			in1 when "00",
+			in2 when "01",
+			in3 when "10",
+			in4 when others;
 
 end behaviour; 
 ----------------------------------------------------------------
